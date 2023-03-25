5964a327be64c33d0326dbc6155516c4c24a3d2f
Fridolin Somers <fridolin.somers@biblibre.com>
2023-03-24 21:31:29 -1000

    Compatible with Koha 22.05 and higher

--- Koha/Plugin/Com/BibLibre/AlwaysShowCheckoutsImmediately.pm
+++ Koha/Plugin/Com/BibLibre/AlwaysShowCheckoutsImmediately.pm
@@ -6,15 +6,15 @@ use base qw(Koha::Plugins::Base);
 
 use C4::Context;
 
-our $VERSION = '1.2';
+our $VERSION = '2.0';
 
 our $metadata = {
     name   => 'Always Show Checkouts Immediately',
     author => 'BibLibre',
     description => 'Always show checkouts immediately in staff interface',
     date_authored   => '2020-07-22',
-    date_updated    => '2022-09-28',
-    minimum_version => '18.11',
+    date_updated    => '2023-03-24',
+    minimum_version => '22.05',
     maximum_version => undef,
     version         => $VERSION,
 };
@@ -59,9 +59,9 @@ sub intranet_js {
 /*
  * Always Show Checkouts Immediately
  */
-if ( typeof script !== 'undefined' && ! $.cookie("issues-table-load-immediately-" + script) ) {
-    $.cookie("issues-table-load-immediately-" + script, true, { expires: 365, path: '/' });
-}
+  if ( typeof script !== 'undefined' && ! Cookies.get("issues-table-load-immediately-" + script) ) {
+    Cookies.set("issues-table-load-immediately-" + script, true, { expires: 365, sameSite: 'Lax' });
+  }
 </script>
 %;
 }
